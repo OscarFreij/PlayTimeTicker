c12c4d5d7e36d0674f65e8b89b0adf1209d12ca9
Oscar Freij <oscar.freij@retune.se>
2021-07-28 18:40:28 +0200

    Initial commit with all code

---
+++ Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace PlayTimeTicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainTable = new System.Windows.Forms.TableLayoutPanel();
+            this.countdownTable = new System.Windows.Forms.TableLayoutPanel();
+            this.clock = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.playerList = new System.Windows.Forms.ListView();
+            this.timesPlayed = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.playerName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.adminTable = new System.Windows.Forms.TableLayoutPanel();
+            this.addNameBtn = new System.Windows.Forms.Button();
+            this.nameInputBox = new System.Windows.Forms.TextBox();
+            this.clockStartStopBtn = new System.Windows.Forms.Button();
+            this.mainTable.SuspendLayout();
+            this.countdownTable.SuspendLayout();
+            this.adminTable.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainTable
+            // 
+            this.mainTable.ColumnCount = 1;
+            this.mainTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.mainTable.Controls.Add(this.countdownTable, 0, 0);
+            this.mainTable.Controls.Add(this.playerList, 0, 1);
+            this.mainTable.Controls.Add(this.adminTable, 0, 2);
+            this.mainTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTable.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.mainTable.Location = new System.Drawing.Point(0, 0);
+            this.mainTable.Name = "mainTable";
+            this.mainTable.RowCount = 3;
+            this.mainTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.mainTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.mainTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.mainTable.Size = new System.Drawing.Size(584, 761);
+            this.mainTable.TabIndex = 0;
+            // 
+            // countdownTable
+            // 
+            this.countdownTable.ColumnCount = 1;
+            this.countdownTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.countdownTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.countdownTable.Controls.Add(this.clock, 0, 0);
+            this.countdownTable.Controls.Add(this.progressBar, 0, 1);
+            this.countdownTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.countdownTable.Location = new System.Drawing.Point(3, 3);
+            this.countdownTable.Name = "countdownTable";
+            this.countdownTable.RowCount = 2;
+            this.countdownTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 71.42857F));
+            this.countdownTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 28.57143F));
+            this.countdownTable.Size = new System.Drawing.Size(578, 144);
+            this.countdownTable.TabIndex = 0;
+            // 
+            // clock
+            // 
+            this.clock.AutoSize = true;
+            this.clock.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.clock.Font = new System.Drawing.Font("MS Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clock.Location = new System.Drawing.Point(3, 0);
+            this.clock.Name = "clock";
+            this.clock.Size = new System.Drawing.Size(572, 102);
+            this.clock.TabIndex = 0;
+            this.clock.Text = "00:00";
+            this.clock.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar.Location = new System.Drawing.Point(3, 105);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(572, 36);
+            this.progressBar.Step = 1;
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 1;
+            // 
+            // playerList
+            // 
+            this.playerList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.timesPlayed,
+            this.playerName,
+            this.date});
+            this.playerList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playerList.HideSelection = false;
+            this.playerList.Location = new System.Drawing.Point(3, 153);
+            this.playerList.Name = "playerList";
+            this.playerList.Size = new System.Drawing.Size(578, 485);
+            this.playerList.TabIndex = 1;
+            this.playerList.TabStop = false;
+            this.playerList.UseCompatibleStateImageBehavior = false;
+            this.playerList.View = System.Windows.Forms.View.Details;
+            // 
+            // timesPlayed
+            // 
+            this.timesPlayed.Text = "Gånger spelat";
+            this.timesPlayed.Width = 83;
+            // 
+            // playerName
+            // 
+            this.playerName.Text = "Namn";
+            this.playerName.Width = 332;
+            // 
+            // date
+            // 
+            this.date.Text = "Datum";
+            this.date.Width = 158;
+            // 
+            // adminTable
+            // 
+            this.adminTable.ColumnCount = 2;
+            this.adminTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 89.44637F));
+            this.adminTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.55363F));
+            this.adminTable.Controls.Add(this.addNameBtn, 0, 1);
+            this.adminTable.Controls.Add(this.nameInputBox, 0, 0);
+            this.adminTable.Controls.Add(this.clockStartStopBtn, 1, 1);
+            this.adminTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.adminTable.Location = new System.Drawing.Point(3, 644);
+            this.adminTable.Name = "adminTable";
+            this.adminTable.RowCount = 2;
+            this.adminTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.adminTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.adminTable.Size = new System.Drawing.Size(578, 114);
+            this.adminTable.TabIndex = 2;
+            // 
+            // addNameBtn
+            // 
+            this.addNameBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.addNameBtn.Location = new System.Drawing.Point(3, 60);
+            this.addNameBtn.Name = "addNameBtn";
+            this.addNameBtn.Size = new System.Drawing.Size(511, 51);
+            this.addNameBtn.TabIndex = 0;
+            this.addNameBtn.Text = "Läggtill namn i listan";
+            this.addNameBtn.UseVisualStyleBackColor = true;
+            this.addNameBtn.Click += new System.EventHandler(this.addNameBtn_Click);
+            // 
+            // nameInputBox
+            // 
+            this.adminTable.SetColumnSpan(this.nameInputBox, 2);
+            this.nameInputBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nameInputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameInputBox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.nameInputBox.Location = new System.Drawing.Point(3, 3);
+            this.nameInputBox.Name = "nameInputBox";
+            this.nameInputBox.Size = new System.Drawing.Size(572, 49);
+            this.nameInputBox.TabIndex = 1;
+            this.nameInputBox.WordWrap = false;
+            // 
+            // clockStartStopBtn
+            // 
+            this.clockStartStopBtn.BackColor = System.Drawing.Color.Orange;
+            this.clockStartStopBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.clockStartStopBtn.Location = new System.Drawing.Point(520, 60);
+            this.clockStartStopBtn.Name = "clockStartStopBtn";
+            this.clockStartStopBtn.Size = new System.Drawing.Size(55, 51);
+            this.clockStartStopBtn.TabIndex = 2;
+            this.clockStartStopBtn.Text = "Starta";
+            this.clockStartStopBtn.UseVisualStyleBackColor = false;
+            this.clockStartStopBtn.Click += new System.EventHandler(this.clockStartStopBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(584, 761);
+            this.Controls.Add(this.mainTable);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Speltids schema";
+            this.mainTable.ResumeLayout(false);
+            this.countdownTable.ResumeLayout(false);
+            this.countdownTable.PerformLayout();
+            this.adminTable.ResumeLayout(false);
+            this.adminTable.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel mainTable;
+        private System.Windows.Forms.TableLayoutPanel countdownTable;
+        private System.Windows.Forms.ListView playerList;
+        private System.Windows.Forms.ColumnHeader timesPlayed;
+        private System.Windows.Forms.ColumnHeader playerName;
+        private System.Windows.Forms.Label clock;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TableLayoutPanel adminTable;
+        private System.Windows.Forms.Button addNameBtn;
+        private System.Windows.Forms.TextBox nameInputBox;
+        private System.Windows.Forms.ColumnHeader date;
+        private System.Windows.Forms.Button clockStartStopBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace PlayTimeTicker
+{
+    /*
+     * PlayTimeTicker
+     * Skapad av Oscar Freij / OffTheGridCG
+     * 2021-07-28
+     */
+
+    public partial class Form1 : Form
+    {
+
+        public static TimeSpan timeToBrick = new TimeSpan(6, 0, 0);
+        public static TimeSpan timePerPlayer = new TimeSpan(0, 40, 0);
+        public static TimeSpan currentPlayerTime = new TimeSpan();
+        public static bool clockRunning = false;
+        public static bool lockdown = false;
+
+        public List<Player> players;
+
+        public Thread clockThread;
+        public Thread killThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            clockThread = new Thread(clockFunction);
+            killThread = new Thread(killFunction);
+            killThread.Start();
+            players = new List<Player>();
+        }
+
+        public void killFunction()
+        {
+            DateTime start = DateTime.Now;
+
+            while (true)
+            {
+                if (timeToBrick <= (DateTime.Now.Subtract(start)))
+                {
+                    lockdown = true;
+                    clockRunning = false;
+
+                    DialogResult res = MessageBox.Show("Nu stänger spelhörnan!", "Notis!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    if (res == DialogResult.OK)
+                    {
+                        // Do Nothing!
+                    }
+                    killThread.Abort();
+                }
+            }
+        }
+
+        public async void clockFunction()
+        {
+            DateTime start = DateTime.Now;
+
+            TimeSpan currentTimeDiff = new TimeSpan();
+
+            while (clockRunning)
+            {
+                currentTimeDiff = (DateTime.Now.Subtract(start));
+
+                SetText((timePerPlayer - (currentPlayerTime + currentTimeDiff)));
+
+                if ((currentPlayerTime.TotalSeconds + currentTimeDiff.TotalSeconds) >= timePerPlayer.TotalSeconds)
+                {
+                    Player currentPlayer = players[0];
+                    players.Remove(currentPlayer);
+                    players.Add(currentPlayer);
+                    currentPlayer.Tick();
+
+                    if (players.Count() != 0)
+                    {
+                        currentPlayerTime = new TimeSpan();
+                        start = DateTime.Now;
+                    }
+                    else
+                    {
+                        SetText(new TimeSpan(0,0,0));
+                        clockRunning = false;
+                        clockThread.Abort();
+                        this.progressBar.MarqueeAnimationSpeed = 0;
+                        this.clockStartStopBtn.Text = "Starta";
+                        this.clockStartStopBtn.BackColor = Color.Orange;
+
+                        return;
+                    }
+                    UpdateList();
+
+                }
+                await Task.Delay(1000);
+            }
+
+            currentPlayerTime = currentPlayerTime + currentTimeDiff;
+            clockThread.Abort();
+        }
+
+        delegate void LockdownCallback();
+        public void Lockdown()
+        {
+            if (this.addNameBtn.InvokeRequired)
+            {
+                LockdownCallback d = new LockdownCallback(Lockdown);
+                this.Invoke(d, new object[] { });
+            }
+            else
+            {
+                this.BackColor = Color.DarkGray;
+                this.addNameBtn.Enabled = false;
+                this.clockStartStopBtn.Enabled = false;
+            }
+        }
+
+        delegate void UpdateListCallback();
+        public void UpdateList()
+        {
+            if (playerList.InvokeRequired)
+            {
+                UpdateListCallback d = new UpdateListCallback(UpdateList);
+                this.Invoke(d, new object[] { });
+            }
+            else
+            {
+                playerList.Items.Clear();
+
+                foreach (var player in players)
+                {
+                    playerList.Items.Add(player.GetListViewItem());
+                }
+
+                playerList.Update();
+            }
+        }
+
+        private void addNameBtn_Click(object sender, EventArgs e)
+        {
+            if (!lockdown)
+            {
+                string data = nameInputBox.Text;
+                players.Add(new Player(data));
+
+                UpdateList();
+                nameInputBox.Clear();
+            }
+        }
+
+        private void clockStartStopBtn_Click(object sender, EventArgs e)
+        {
+            if (!lockdown)
+            {
+                if (!clockRunning && players.Count > 0)
+                {
+                    clockRunning = true;
+                    clockThread = new Thread(clockFunction);
+                    clockThread.Start();
+                    this.progressBar.MarqueeAnimationSpeed = 500;
+                    this.clockStartStopBtn.Text = "Pausa";
+                    this.clockStartStopBtn.BackColor = Color.LimeGreen;
+                }
+                else
+                {
+                    clockRunning = false;
+                    clockThread.Abort();
+                    this.progressBar.MarqueeAnimationSpeed = 0;
+                    this.clockStartStopBtn.Text = "Starta";
+                    this.clockStartStopBtn.BackColor = Color.Orange;
+                }
+            }
+        }
+
+        delegate void SetTextCallback(TimeSpan ts);
+
+        private void SetText(TimeSpan ts)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.clock.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { ts });
+            }
+            else
+            {
+                int percentLeft = (int)((ts.TotalSeconds / timePerPlayer.TotalSeconds) * 100);
+                if ( percentLeft >= 0)
+                {
+                    this.progressBar.Value = percentLeft;
+                }
+
+                this.clock.Text = $"{ts.Minutes}:{ts.Seconds}";
+
+                if (ts.Seconds < 5)
+                {   
+                    this.progressBar.SetState(2);
+                }
+                else if (ts.Seconds < 15)
+                {
+                    this.progressBar.SetState(3);
+                }
+                else
+                {
+                    this.progressBar.SetState(1);
+                }
+            }
+        }
+    }
+
+    public static class ModifyProgressBarColor
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr w, IntPtr l);
+        public static void SetState(this ProgressBar pBar, int state)
+        {
+            SendMessage(pBar.Handle, 1040, (IntPtr)state, IntPtr.Zero);
+        }
+    }
+
+    public class Player
+    {
+        public string name;
+        public int cycleTime;
+        public DateTime lastPlayed;
+        public Player(string name)
+        {
+            this.name = name;
+            this.cycleTime = 0;
+        }
+
+        public void Tick()
+        {
+            cycleTime++;
+            lastPlayed = DateTime.Now;
+        }
+
+        public ListViewItem GetListViewItem()
+        {
+            string lPData = "-----";
+
+            if (lastPlayed != new DateTime())
+            {
+                lPData = lastPlayed.ToString();
+            }
+            string[] data = { cycleTime.ToString(), name, lPData };
+            return new ListViewItem(data);
+        }
+    }
+}
